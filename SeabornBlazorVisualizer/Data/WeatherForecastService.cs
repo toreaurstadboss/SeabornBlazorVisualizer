85d53d8aa71c5b7a717e0f86fa8ce3ededec4285
Tore Aurstad <tore.aurstad@gmail.com>
2025-04-29 02:02:35 +0200

    More work on avoiding freeze

--- SeabornBlazorVisualizer/Data/WeatherForecastService.cs
+++ SeabornBlazorVisualizer/Data/WeatherForecastService.cs
@@ -7,13 +7,20 @@ namespace SeabornBlazorVisualizer.Data
 {
     public class WeatherForecastService
     {
+
+        static bool runtime_initialized = false;
+
         static WeatherForecastService()
         {
+
             string pythonDll = @"C:\Users\Tore\AppData\Local\Programs\Python\Python310\python310.dll";
             // Set environment variables
             Environment.SetEnvironmentVariable("PYTHONHOME", @"C:\programdata\anaconda3", EnvironmentVariableTarget.Process);
             Environment.SetEnvironmentVariable("PYTHONPATH", @"c:\programdata\anaconda3\lib\site-packages", EnvironmentVariableTarget.Process);
             Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", pythonDll);
+
+            //Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", @"C:\ProgramData\Anaconda3\python310.dll");
+            Environment.SetEnvironmentVariable("PYTHONNET_PYVER", "3.10");
 
             PythonEngine.Initialize();
         }
@@ -62,17 +69,24 @@ namespace SeabornBlazorVisualizer.Data
           
             using (Py.GIL()) //Python Global Interpreter Lock (GIL)
             {
+                if (!runtime_initialized)
+                {
+                    dynamic sys = Py.Import("sys");
+                    sys.path.append(@"C:\ProgramData\Anaconda3\Lib\site-packages");
+                    Console.WriteLine(sys.path);
 
-                dynamic sys = Py.Import("sys");
-                sys.path.append(@"C:\ProgramData\Anaconda3\Lib\site-packages");
-                Console.WriteLine(sys.path);
+                    //add folders in solution too
+
+                    sys.path.append(@"Data/");
+                    runtime_initialized = true;
+                }
 
                 Py.Import("numpy");
                 //Py.Import("matplotlib");
                // Py.Import("seaborn");
                
 
-                sys.path.append(@"Data/");
+              
                 dynamic script = Py.Import(@"hello");
 
                 result = script.InvokeMethod("get_hello_world");
